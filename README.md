f681e40e0bed092aa11ee580d986c9bf6ebf10af
johnqing <csssnow@gmail.com>
2013-04-27 15:17:18 +0800

    new

--- README.md
+++ README.md
@@ -5,5 +5,7 @@
 
 目前支持2个参数
 
++ id 唯一标识符
 + class 日历控件UI层样式
 + eventType 点击控件执行函数（暂未实现）
++ callback 拥有4个参数 object、year、month、day
--- calender.js
+++ calender.js
@@ -6,6 +6,18 @@
         for (var property in source) define[property] = source[property];
         return define;
     },
+    /**
+    * 给目标元素绑定事件
+    * @name _CAL#on
+    * @param {object} element		目标元素
+    * @param {string} type 		事件类型
+    * @param {function} handler	处理函数
+    * @function
+    */
+    on = function(element, type, handler) {
+        element.addEventListener ? element.addEventListener(type, handler, false) : element.attachEvent("on" + type, handler);
+    },
+    get
     isYYear = function(year){
         if(year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)){
             return true;
@@ -13,8 +25,12 @@
         return false;
     };
     var defaultConfig = {
+        id: 'nsCal',
         class: 'calender',
-        eventType: 'click'
+        eventType: 'click',
+        callback: function(){
+
+        }
     };
     /**
      * Class
@@ -22,8 +38,10 @@
      * @constructor
      */
     var Calender = function(o){
+        this.id = o.id;
         this.class = o.class;
         this.eventType = o.eventType;
+        this.callback = o.callback;
         this.week = ['日', '一', '二', '三', '四', '五', '六']
         this.init();
     }
@@ -31,9 +49,11 @@
         init: function(){
             var tb = this.createTb();
             var wrap = doc.createElement('div');
+            wrap.id = this.id;
             wrap.className = this.class;
             wrap.innerHTML = tb;
             body.appendChild(wrap);
+            this.addEvent(wrap);
         },
         /**
          * 当前时间信息
@@ -119,14 +139,14 @@
                 tb += '<tr>';
                 for(var i = 0; i< 7; i++){
                     if((i*j+i+j*7) < first){
-                        tb += '<td align="center">&nbsp;</td>';
+                        tb += '<td align="center" class="disabled">&nbsp;</td>';
                     }else{
                         if(day <= days){
                             var curClass = day === cur.d ? 'current' : '';
                             tb += '<td align="center" id="'+day+'" class="'+curClass+'"><a href="###">'+day+'</a></td>';
                             day++;
                         }else{
-                            tb += '<td align="center">&nbsp;</td>';
+                            tb += '<td align="center" class="disabled">&nbsp;</td>';
                         }
                     }
                 }
@@ -136,6 +156,18 @@
             //end
             tb += '</table>';
             return tb;
+        },
+        addEvent: function(obj){
+            var cur = this.getCurDy(),
+                self = this;
+            on(obj, this.eventType, function(e){
+                e = e || win.event;
+                var oTarget = e.target || e.srcElement;
+                oTarget.parentNode.tagName.toUpperCase() === "A" && (oTarget = oTarget.parentNode);
+                if(oTarget.tagName.toUpperCase() === "A" && oTarget.className != "disabled") {
+                    self.callback(obj, cur.y, cur.m, oTarget.innerHTML);
+                }
+            });
         }
     }
     /**
